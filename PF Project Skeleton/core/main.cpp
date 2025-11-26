f3f675f190fda1647282c419c13d1b7851c49e61
Muhammad Ibrahim <m.ibraheem.isb@gmail.com>
2025-11-27 01:18:04 +0500

    Updated code (worked in io.cpp)

--- PF Project Skeleton/core/main.cpp
+++ PF Project Skeleton/core/main.cpp
@@ -4,46 +4,45 @@
 #include <iostream>
 using namespace std;
 
-// Function to print the grid
 void printGrid()
 {
     for (int r = 0; r < rows; r++)
     {
         for (int c = 0; c < cols; c++)
-        {
             cout << grid[r][c];
-        }
         cout << endl;
     }
     cout << "--------------------------\n";
 }
 
+void printSwitches()
+{
+    cout << "\n=== SWITCH DEBUG ===\n";
+    for (int i = 0; i < total_switches; i++)
+    {
+        cout << "Switch " << i
+             << " Letter: " << char('A' + i)
+             << " Row: " << switch_x[i]
+             << " Col: " << switch_y[i]
+             << " State: " << switch_state[i]
+             << endl;
+    }
+    cout << "====================\n\n";
+}
+
 int main()
 {
-    initializeSimulationState();   // reset everything
-    initializeLogFiles();          // create empty CSV logs
+    initializeSimulationState();
+    initializeLogFiles();
 
     if (!loadLevelFile())
     {
-        cout << "Error loading level file!\n";
+        cout << "Error loading level file\n";
         return 0;
     }
 
-    // SIMPLE LOOP (run 20 ticks)
-    for (int t = 0; t < 20; t++)
-    {
-        currentTick = t;
-
-        // CALL YOUR LOGGING FUNCTIONS
-        logTrainTrace();
-        logSwitchState();
-        logSignalState();
-
-        // PRINT GRID ON SCREEN
-        printGrid();
-    }
-
-    writeMetrics();  // write final summary
-
+    printSwitches();
+    printGrid();
+    writeMetrics();
     return 0;
 }
