dad720a8d29c3fbe0e15b680cd6b8073bc9205ad
MZiaRAwan <i250040@isb.nu.edu.pk>
2025-11-30 01:44:23 +0500

    Updated grid.cpp

--- PF Project Skeleton/core/grid.cpp
+++ PF Project Skeleton/core/grid.cpp
@@ -1,5 +1,8 @@
 #include "grid.h"
 #include "simulation_state.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
 
 bool isInBounds(int r, int c) {
@@ -37,4 +40,68 @@ bool isDestinationPoint(char tile)
 bool toggleSafetyTile(char tile)
  {
  return (tile == '#' || tile == '.');
+ }
+
+// ----------------------------------------------------------------------------
+// PRINT GRID WITH TRAINS (Terminal Output - Phase 7)
+// ----------------------------------------------------------------------------
+// Print complete grid state showing all tiles, train positions, and switch states.
+// Required: Terminal output at each tick showing grid state.
+// ----------------------------------------------------------------------------
+void printGrid()
+{
+    // Create a display grid that includes trains
+    char display_grid[max_rows][max_cols];
+    
+    // Copy base grid
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < cols; c++)
+        {
+            display_grid[r][c] = grid[r][c];
+        }
+    }
+    
+    // Overlay trains on the grid (use train ID as character)
+    for (int i = 0; i < total_trains; i++)
+    {
+        if (train_active[i] && isInBounds(train_x[i], train_y[i]))
+        {
+            // Use train ID (0-9) or letter for display
+            if (i < 10)
+                display_grid[train_x[i]][train_y[i]] = '0' + i;
+            else
+                display_grid[train_x[i]][train_y[i]] = 'A' + (i - 10);
+        }
+    }
+    
+    // Print tick number
+    cout << "\nTick: " << currentTick << "\n";
+    
+    // Print grid
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < cols; c++)
+        {
+            cout << display_grid[r][c];
+        }
+        cout << endl;
+    }
+    
+    // Print train information
+    for (int i = 0; i < total_trains; i++)
+    {
+        if (train_active[i])
+        {
+            string dir_str = "UNKNOWN";
+            if (train_dir[i] == DIR_UP) dir_str = "UP";
+            else if (train_dir[i] == DIR_RIGHT) dir_str = "RIGHT";
+            else if (train_dir[i] == DIR_DOWN) dir_str = "DOWN";
+            else if (train_dir[i] == DIR_LEFT) dir_str = "LEFT";
+            
+            cout << "Train " << i << " at (" << train_x[i] << "," << train_y[i] << ") moving " << dir_str << "\n";
+        }
+    }
+    
+    cout << "--------------------------\n";
 }
